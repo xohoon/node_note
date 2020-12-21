2a8e8c1bfe0daa04c7f9b1de423dc22224086b1a
xohoon <xohoonx2@gmail.com>
2020-12-21 17:29:56 +0900

    ERROR UPDATE

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(flash());
 app.use('/', pageRouter);
 
 app.use((req, res, next) => {
-    const err = new Error('Not Found');
+    const err =  new Error('Not Found');
     err.status = 404;
     next(err);
 });
